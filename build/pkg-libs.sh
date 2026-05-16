e5430082ad6f4564d420452020d488689dec9c34
OkaVatti <okavatti@proton.me>
2026-05-15 23:54:24 -0400

    fixed build scripts Apply crystal tool format

--- build/pkg-libs.sh
+++ build/pkg-libs.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-set -e
-
-# Shard directory passed as first argument when called from lib_sodium.cr
-[ ! -z "$1" ] && cd "$1"
-
-. ./build/env.sh
-
-pkg-config libsodium --libs
+#!/bin/sh
+echo "-lsodium"
