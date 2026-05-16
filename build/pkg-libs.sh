334ed6b794c0537015c0b8e61db0ff665458bf31
OkaVatti <okavatti@proton.me>
2026-05-16 00:01:31 -0400

    Revert crystal tool format - formatter broke line breaks

--- build/pkg-libs.sh
+++ build/pkg-libs.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-echo "-lsodium"
+#!/bin/bash
+set -e
+
+# Shard directory passed as first argument when called from lib_sodium.cr
+[ ! -z "$1" ] && cd "$1"
+
+. ./build/env.sh
+
+pkg-config libsodium --libs
